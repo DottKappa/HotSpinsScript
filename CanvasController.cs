f43d6e948c1aba8b2472085572a7210ef11bcb4b
DottKappa <kappaalberto@libero.it>
2025-03-20 13:01:04 +0100

    [MAIN] Aggiunta visibilità del numero di spin

--- CanvasController.cs
+++ CanvasController.cs
@@ -1,7 +1,19 @@
 using UnityEngine;
+using TMPro;
 
 public class CanvasController : MonoBehaviour
 {
+    public TextMeshProUGUI numberOfSlotsText;
+    private SceneManager sceneManager;
+
+    void Start() {
+        sceneManager = FindFirstObjectByType<SceneManager>();
+    }
+
+    void Update() {
+        numberOfSlotsText.text = "Spins: " + addDot(sceneManager.GetNumberOfSpins());
+    }
+
     public void ToggleCanvasElements(bool isActive)
     {
         foreach (Transform child in transform) {
@@ -16,5 +28,27 @@ public class CanvasController : MonoBehaviour
                 Destroy(powerUp);
             }
         }
+    }
+
+    private string addDot(int points)
+    {
+        string pointsString = points.ToString();
+        int length = pointsString.Length;
+
+        if (length <= 3) return pointsString;
+
+        System.Text.StringBuilder sb = new System.Text.StringBuilder();
+        int counter = 0;
+
+        for (int i = length - 1; i >= 0; i--) {
+            sb.Insert(0, pointsString[i]);
+            counter++;
+            if (counter == 3 && i != 0) {
+                sb.Insert(0, '.');
+                counter = 0;
+            }
+        }
+
+        return sb.ToString();
     }
 }
